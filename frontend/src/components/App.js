279d7bb6d84e9bc3c2397a197807ad9cea36a8bf
Przemyslaw Babula <53541810+przemek-babula@users.noreply.github.com>
2020-09-08 19:50:45 +0200

    files tree rebiuld (#1)
    * files tree rebiuld
    
    * contenerization + webpack
    
    * merge master
    
    Co-authored-by: przemek <przemek@pop-os.localdomain>

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+// import "./App.css";
 import LoginPanel from "./LoginPanel";
 import MoodTracker from "./MoodTracker";
 import SigIn from "./SigIn";
