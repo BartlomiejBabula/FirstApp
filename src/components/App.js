1befc57fb195f1fc57576280fc21cdc6175ddaa0
Bartlomiej Babula <babula.bartlomiej@gmail.com>
2020-09-08 19:22:19 +0200

    add login panel, add rauting

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,26 @@
 import React from "react";
-import "./App.scss";
+import "./App.css";
+import LoginPanel from "./LoginPanel";
+import MoodTracker from "./MoodTracker";
+import SigIn from "./SigIn";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 
-function App() {
-  return (
-    <>
-      <p>QUPA</p>
-    </>
-  );
+class App extends React.Component {
+  state = {
+    access: false,
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Route path="/" exact component={LoginPanel} />
+          <Route path="/homepage" component={MoodTracker} />
+          <Route path="/sigin" component={SigIn} />
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
